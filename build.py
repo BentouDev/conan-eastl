e6b6c14c284489275a75ebaa601b1cc9adabc70a
BentouDev <bentkowski.jakub@gmail.com>
2019-04-18 22:21:16 +0200

    First version

---
+++ build.py
@@ -0,0 +1,137 @@
+#!/bin/python3
+from cpt.packager import ConanMultiPackager
+from conans import tools
+import os, sys
+
+DEBUG_MODE = False
+GIT_DIR = "eastl-source"
+STABLE_IN_GIT = True
+
+def createBuilder(channel, commit, password, version):
+    branch_pattern = 'release*' # channel is set explicitly!
+    username = "bentoudev"
+
+    if not "CONAN_VISUAL_VERSION" in os.environ:
+        visual_versions = ["15","16"]
+    else:
+        visual_versions = [os.environ["CONAN_VISUAL_VERSION"]]
+
+    if password:
+        return ConanMultiPackager(username=username,
+                channel=channel,
+                stable_branch_pattern=branch_pattern,
+                visual_versions=visual_versions,
+
+                upload="https://api.bintray.com/conan/bentoudev/eastl",
+                password=password)
+    else:
+        return ConanMultiPackager(username=username,
+                channel=channel,
+                stable_branch_pattern=branch_pattern,
+                visual_versions=visual_versions)
+
+def build(channel, commit, password, version):
+
+    os.environ['EASTL_COMMIT'] = commit
+    os.environ['EASTL_VERSION'] = version
+
+    builder = createBuilder(channel, commit, password, version)
+
+    builder.add_common_builds()
+    builder.add(settings={"arch": "x86_64", "build_type": "Release"},
+                options={},
+                env_vars={},
+                build_requires={})
+
+    builder.run()
+
+def runCommand(args):
+    import subprocess
+    if len(args) == 0:
+        return None
+    try:
+        if DEBUG_MODE:
+            print(' [*] Running cmd... ' + args[0])
+        raw_out = subprocess.check_output(args, shell=False, cwd=GIT_DIR)
+
+        stdout = raw_out.decode()
+
+        #cmd = run(args, encoding='utf-8', stdout=PIPE)
+        #cmd.check_returncode()
+
+        out = stdout.split('\n')
+        if DEBUG_MODE:
+            print(" [*] stdout: " + out[0])
+            for x in range(len(out)):
+                print(str(x) + ': ' + out[x])
+
+        return str(out[0])
+    except Exception as error:
+        print (' [*] Caught error: ' + str(error))
+        return None
+
+def getGitVersion():
+    try:
+        data = {}
+        data['version'] = runCommand(['git', 'describe'])
+        data['commit'] = runCommand(['git', 'rev-parse', 'HEAD'])
+        return data
+    except Exception as error:
+        print (' [*] Caught error: ' + str(error))
+        return None
+
+def execute(password):
+    channel = 'dev'
+    version = None
+    commit = None
+
+    if 'CI' in os.environ:
+        print(' [info] CI Environment detected')
+
+        if 'APPVEYOR' in os.environ:
+            print(" [info] Welcome, AppVeyor!")
+            if 'APPVEYOR_REPO_TAG_NAME' in os.environ:
+                version = os.environ['APPVEYOR_REPO_TAG_NAME']
+            if 'APPVEYOR_REPO_COMMIT' in os.environ:
+                commit = os.environ['APPVEYOR_REPO_COMMIT']
+
+        if 'TRAVIS' in os.environ:
+            print(" [info] Welcome, Travis!")
+            os.environ['CXX'] = 'clang++-5.0'
+            os.environ['CC'] = 'clang-5.0'
+            if 'TRAVIS_TAG' in os.environ:
+                version = os.environ['TRAVIS_TAG']
+            if 'TRAVIS_COMMIT' in os.environ:
+                commit = os.environ['TRAVIS_COMMIT']
+
+    if not version or not commit:
+        print (' [*] Attempt to get version from git...')
+        gitData = getGitVersion()
+        if gitData:
+            version = gitData['version']
+            commit = gitData['commit']
+        if STABLE_IN_GIT:
+            channel = 'stable'
+    else:
+        channel = 'stable'
+
+    if version and commit:
+        print (' [info] Channel: ' + channel)
+        print (' [info] Version: ' + version)
+        print (' [info] Commit: ' + commit)
+        print ('')
+        print (' [*] Executing conan build...')
+
+        build(channel, commit, password, version)
+    else:
+        msg = "Unable to determine version"
+        sys.stderr.write(str.format(' [error] {}!', msg))
+        raise Exception(msg)
+
+if __name__ == '__main__':
+    print ('')
+    if len(sys.argv) >= 2:
+        execute(sys.argv[1])
+    else:
+        print(" [warn] Missing repository key argument! Package won't be uploaded")
+        execute(None)
---
+++ conanfile.py
@@ -0,0 +1,43 @@
+from conans import ConanFile, CMake
+import os
+
+eastl_version = os.getenv('EASTL_VERSION', '0.0')
+eastl_commit = os.getenv('EASTL_COMMIT', '')
+
+class EASTLConan(ConanFile):
+    name = "EASTL"
+    license = "MIT"
+    url = "https://github.com/BentouDev/conan-eastl"
+    version = eastl_version
+    commit = eastl_commit
+
+    description = "EASTL stands for Electronic Arts Standard Template Library. It is an extensive and robust implementation that has an emphasis on high performance."
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    exports_sources = ["eastl-source/*"]
+
+    options = {"build_type": ["Release", "Debug", "RelWithDebInfo", "MinSizeRel"]}
+    default_options = "build_type=MinSizeRel",
+
+    def package_id(self):
+        self.info.include_build_settings()
+        del self.info.settings.compiler
+        del self.info.settings.arch
+        del self.info.settings.build_type
+
+    def build(self):
+        cmake = CMake(self)
+        #cmake.definitions['EASTL_VERSION'] = self.version
+        #cmake.definitions['EASTL_COMMIT'] = self.commit
+        #cmake.definitions['EASTL_CHANNEL'] = self.channel
+        cmake.configure(source_folder="eastl-source")
+        cmake.build()
+
+    def package(self):
+        self.copy("*.h", src="eastl-source/test/packages/EABase/include/Common/EABase", dst="include/EABase", keep_path=True)
+        self.copy("*.h", src="eastl-source/include", dst="include", keep_path=True)
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = [ self.name ]
