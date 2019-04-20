774215392a47940eef375eba64d812759da2f04b
BentouDev <bentkowski.jakub@gmail.com>
2019-04-20 16:30:32 +0200

    Cleanup

--- setup_vs.ps1
+++
@@ -1,4 +0,0 @@
-if ($env:CONAN_VISUAL_VERSIONS -eq 16)
-{
-    choco install visualstudio2019buildtools --package-parameters "--add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --quiet --locale en-US" --confirm
-}
