0a71c8e17a07c7d25aaadf2f9eebb95d453f2fd3
BentouDev <bentkowski.jakub@gmail.com>
2019-04-21 10:11:34 +0200

    Azure: remove env set from script

--- build_azure.bat
+++ build_azure.bat
@@ -5,8 +5,4 @@ pip install conan conan_package_tools --upgrade --upgrade-strategy only-if-neede
 
 conan user
 move conan-fallback-settings.yml %USERPROFILE%/.conan/settings.yml
-set CONAN_VISUAL_VERSIONS=16
-set CI=true
-set AZURE=true
-set AZURE_BUILD_NUMBER=$(Build.BuildNumber)
 python build.py
