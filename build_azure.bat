3ded945e90772f7322dc2182ecccfe2d41e897a4
BentouDev <bentkowski.jakub@gmail.com>
2019-04-21 10:16:45 +0200

    Azure: Attempt to force usage of newer cmake

--- build_azure.bat
+++ build_azure.bat
@@ -1,8 +1,11 @@
 python --version
 pip --version
 
+choco upgrade cmake
+
 pip install conan conan_package_tools --upgrade --upgrade-strategy only-if-needed
 
 conan user
 move conan-fallback-settings.yml %USERPROFILE%/.conan/settings.yml
+cmake --version
 python build.py
