5ab2e9dc3e2798774634f9bcc5bcbf711c1a55ae
BentouDev <bentkowski.jakub@gmail.com>
2019-04-21 10:27:06 +0200

    Azure: list cmake path

--- build_azure.bat
+++ build_azure.bat
@@ -1,11 +1,9 @@
-python --version
-pip --version
-
-choco upgrade cmake
+cmake --version
+where cmake
 
 pip install conan conan_package_tools --upgrade --upgrade-strategy only-if-needed
 
 conan user
 move conan-fallback-settings.yml %USERPROFILE%/.conan/settings.yml
-cmake --version
+
 python build.py
