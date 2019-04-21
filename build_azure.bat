c7ae93d09a7e2ad5a73889c7a3b183a96d44e2dc
BentouDev <bentkowski.jakub@gmail.com>
2019-04-21 09:57:56 +0200

    Update azure script

--- build_azure.bat
+++ build_azure.bat
@@ -1,8 +1,8 @@
 python --version
 pip --version
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "$root = ((New-Object System.Net.WebClient).DownloadString('https://bootstrap.pypa.io/get-pip.py')) | python $root"
+
 pip install conan conan_package_tools --upgrade --upgrade-strategy only-if-needed
-refreshenv
+
 conan user
 move conan-fallback-settings.yml %USERPROFILE%/.conan/settings.yml
 python build.py
