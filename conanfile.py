930c9fadb2f4ae21dc901fa0c260c2abf8c70900
BentouDev <1926444+BentouDev@users.noreply.github.com>
2019-05-08 17:01:19 +0200

    No build_type in options

--- conanfile.py
+++ conanfile.py
@@ -16,8 +16,8 @@ class EASTLConan(ConanFile):
     generators = "cmake"
     exports_sources = ["eastl-source/*"]
 
-    options = {"build_type": ["Release", "Debug", "RelWithDebInfo", "MinSizeRel"]}
-    default_options = "build_type=MinSizeRel",
+    options = {}
+    default_options = ""
 
     def package_id(self):
         self.info.include_build_settings()
